b03aeac9c789d224084f35e1ab85c723d812e5df
Minji <321963741@naver.com>
2021-10-24 16:02:48 +0900

    13 project done

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const sliders = document.querySelectorAll('.slide-in');
+
 function debounce(func, wait = 20, immediate = true) {
   var timeout;
   return function () {
@@ -13,3 +15,19 @@ function debounce(func, wait = 20, immediate = true) {
     if (callNow) func.apply(context, args);
   };
 }
+
+const handleScroll = () => {
+  sliders.forEach((slider) => {
+    const slideHeight = window.innerHeight + window.scrollY - slider.height / 2;
+    const imgBottom = slider.offsetTop + slider.height;
+    const isShownBottomImg = slideHeight > slider.offsetTop;
+    const isNotShownPastImg = window.scrollY < imgBottom;
+    if (isShownBottomImg && isNotShownPastImg) {
+      slider.classList.add('active');
+    } else {
+      slider.classList.remove('active');
+    }
+  });
+};
+
+window.addEventListener('scroll', handleScroll);
